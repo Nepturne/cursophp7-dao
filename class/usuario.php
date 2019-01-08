ad807640a94165545c0e7057092ab4b982c3a5e4
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2019-01-08 01:02:35 -0300

    Funções diversas em listar ou recuperar dados com DAO

--- class/usuario.php
+++ class/usuario.php
@@ -3,13 +3,14 @@
 
 class Usuario {
 
+	#-> Atributos/Encapsulamento:-------------------------------------------------------------------------------------
 	private $idusuario;
 	private $deslogin;
 	private $dessenha;
 	private $dtcadastro;
 
 	#-> Getters and Setters:
-	#=====================================
+	#==================================================================================================================
 	public function getIdUsuario(){
 		return $this->idusuario;
 	}
@@ -17,7 +18,7 @@ class Usuario {
 	public function setIdUsuario($value){
 		$this->idusuario = $value;
 	}
-    #=====================================
+    #==================================================================================================================
 	public function getDesLogin(){
 		return $this->deslogin;
 	}
@@ -25,7 +26,7 @@ class Usuario {
 	public function setDesLogin($value){
 		$this->deslogin = $value;
 	}
-	#=====================================
+	#==================================================================================================================
 	public function getDesSenha(){
 		return $this->dessenha;
 	}
@@ -33,7 +34,7 @@ class Usuario {
 	public function setDesSenha($value){
 		$this->dessenha = $value;
 	}
-	#=====================================
+	#==================================================================================================================
 	public function getDtCadastro(){
 		return $this->dtcadastro;
 	}
@@ -41,9 +42,9 @@ class Usuario {
 	public function setDtCadastro($value){
 		$this->dtcadastro = $value;
 	}
-	#=====================================
+	#==================================================================================================================
 
-	# Métodos: -----------------------------------------------------------------------------------------------------
+	# Métodos Individuais da Classe: ----------------------------------------------------------------------------------
 	
 	#-> Função loadByID => Recebe um Id e utiliza a classe sql para utilizar o método select que recebe 2 paramêtros
 	#-> o primeiro paramêtro é a rowQuery e o segundo os parâmetros  onde um array define chave valor para que o identificador
@@ -68,11 +69,63 @@ class Usuario {
 			$this->setDesSenha($row['dessenha']);
 			$this->setDtCadastro(new DateTime($row['dtcadastro']));
 
+		} else{
+			echo 'Nenhum identificador passado como parâmetro para retornar resultados.';
 		}
 
 
 	}
 
+	#-> Retorna todos os usuários listados pelo login salvo no banco como (deslogin).
+	#-> Método Statico não precisa de instancia em objeto somente nomeDaClasse::função();
+	public static function getList(){
+
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+	}
+
+	#-> O que digitar dentro do paramêtro $login será consultado no banco utilizando %% para não diferenciar maisc. e minusc.
+	public static function search($login){
+		
+		$sql = new Sql();
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :SEARCH ORDER BY deslogin", array(
+
+			':SEARCH'=>'%'.$login.'%'
+
+		));
+
+	}
+
+	#-> Verifica se os valores passados como parâmetros estão iguais aos cadastrados no banco caso sim conecta caso não
+	#-> executa um throw new Exception("login e/ou Senha inválidos");
+	public function login($login,$password){
+
+		$sql = new Sql();
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :SENHA",array(  
+
+		":LOGIN" => $login,
+		":SENHA" => $password
+
+		));
+
+		if(count($results) > 0){
+
+			$row = $results[0];
+			$this->setIdUsuario($row['idusuario']);
+			$this->setDesLogin($row['deslogin']);
+			$this->setDesSenha($row['dessenha']);
+			$this->setDtCadastro(new DateTime($row['dtcadastro']));
+
+		} else{
+			throw new Exception("Login e/ou Senha inválidos");
+			
+		}
+
+	}
+
+	#-> Executa no momento em que o echo é feito no objeto retornando os gets apos os sets atribuidos pelas métodos com set.
+	#-> No formato índice (nome do campo)=>$this->getValue() = Valor do campo.
 	public function __toString(){
 		
 		return json_encode(array(
@@ -84,12 +137,8 @@ class Usuario {
 
 	}
 
-
-
-
-
-
-}
+	
+} //-> FIM DA CLASSE
 
 
 
--- index.php
+++ index.php
@@ -2,19 +2,37 @@
 #-> DAO - Data Acess Objetct:
 require_once('config.php');
 
-
 # A classe sql que trata conexões e formas de Select, Update, Delete de dados vai ser consumida pelas classes que a utilizam.
 # A classe usuario utiliza a classe sql e retorna com as funções sql resultados atribuindo a atributos com set e retornando
 # com o get.
 
+/* UTILIZANDO A CLASSE SQL DE FORMA DIRETA E NãO INDIRETA COMO POR EXEMPLO PELA CLASSE USUARIO.PHP (SQL SECO)
 # $sql = new Sql();
 
 # $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
-# echo json_encode($usuarios);
+# echo json_encode($usuarios); */
 
-$root = new Usuario();
-$root->loadById(1);
-echo $root;
+# Carrega um usuário com base em seu Id passado como parâmetro setando suas informações no banco nas row com os sets e 
+# recuperando-as com os gets; (CARREGA PELO ID)
+# $root = new Usuario();
+
+# $root->loadById(1);
+
+# echo $root;
+
+# Carrega uma lista de usuários com o método stático getList(); (LISTA ESTÁTICA)
+# $lista = Usuario::getList();
+
+# echo json_encode($lista);
+
+# Carrego uma lista de usuários pesquisando pelo login. (SEARCH)
+$afk = new Usuario::search('user');
+echo json_encode($afk);
+
+# Carrega usuário se o login e senha estiverem corretas como no banco de dados.
+# $usuario = new Usuario();
+# $usuario->login("lucasroot","12345679998");
+# echo $usuario;
 
  ?>
