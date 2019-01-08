4d19e4af7eca0a436c92be77b4e2584067948368
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2019-01-07 23:52:43 -0300

    Comentários Necessários Entendimento

--- class/usuario.php
+++ class/usuario.php
@@ -45,6 +45,13 @@ class Usuario {
 
 	# Métodos: -----------------------------------------------------------------------------------------------------
 	
+	#-> Função loadByID => Recebe um Id e utiliza a classe sql para utilizar o método select que recebe 2 paramêtros
+	#-> o primeiro paramêtro é a rowQuery e o segundo os parâmetros  onde um array define chave valor para que o identificador
+	#-> :ID referencie $id ( parâmetro passado na função loadById() ). Caso a variável que armazena o select $results
+	#-> retorne mais que 0 no caso 1 registro pois o id foi passado para retornar 1. 
+	#-> $results[0] = Que representa o primeiro registro retornado atribui com sets os idusuario,deslogin...
+	#-> E o método mágico __toString retorna em json um array com os indices e valores que retornam nos gets os valores
+	#-> setados anteriormente no loadById().
 	public function loadById($id){
 		$sql = new Sql();
 		$results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID",array(  
@@ -74,7 +81,7 @@ class Usuario {
 			"dessenha"=>$this->getDesSenha(),
 			"dtcadastro"=>$this->getDtCadastro()->format("d/m/Y H:i:s")
 		));
-		
+
 	}
 
 
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php 
-
+#-> DAO - Data Acess Objetct:
 require_once('config.php');
 
+
+# A classe sql que trata conexões e formas de Select, Update, Delete de dados vai ser consumida pelas classes que a utilizam.
+# A classe usuario utiliza a classe sql e retorna com as funções sql resultados atribuindo a atributos com set e retornando
+# com o get.
 
 # $sql = new Sql();
 
